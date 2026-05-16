fad43d34e77f926c7d1bfb1f360906fd0c1b9b03
25SUB5935_U26 <25SUBB5935_U26@rpsconsulting.com>
2026-05-16 09:51:01 +0530

    second commit

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask,request
 from flask_restful import Api,Resource,reqparse,fields,marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///c:/Users/Administrator/userresource.db"
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///"+os.path.join(basedir,"userresource.db")
 CORS(app)
 api=Api(app)
 db=SQLAlchemy(app)
